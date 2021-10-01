619004b7812d5313e417ae6c4814636eded9737e
rimunanda-15 <arimrimunanda15@gmail.com>
2021-10-01 14:10:49 +0700

    Solusi jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+			System.out.println("NIM		: 670621310");
+			System.out.println("Nama	: Rimunanda Ma'arif Parinduri");
+			System.out.println("Kelas	: D3IF -45-04 ");	
+	}
+}
